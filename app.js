56318f28efe6c6b08562700e32e3bd98d885e9b2
tienhigo <tienhigo@gmail.com>
2024-06-17 03:13:40 +0700

    first cicd

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const app = express();
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+
 
 app.get('/name', (req, res) => {
     res.send("Telmo");
