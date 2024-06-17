1443526e448cc4f80fca07603d033b30106b2de4
tienhigo <tienhigo@gmail.com>
2024-06-18 00:17:57 +0700

    hh

--- frontend/src/components/todo.js
+++ frontend/src/components/todo.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-function todo({ todo }) {
-    const { id, title, completed } = todo;
-    const h1 = <h1>{title}</h1>;
-    const text = completed ? <strike>{h1}</strike> : h1;
-    return <div data-testid={`todo-${id}`}>{text}</div>
+// function todo({ todo }) {
+//     const { id, title, completed } = todo;
+//     const h1 = <h1>{title}</h1>;
+//     const text = completed ? <strike>{h1}</strike> : h1;
+//     return <div data-testid={`todo-${id}`}>{text}</div>
+// }
+function todo({}){
+    return <div data-testid="todo-1">TODO LIST</div>
 }
 export default todo;
