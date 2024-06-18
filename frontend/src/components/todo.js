8fbe0e1ad9d39055b7ae904a0085f88e22a8becc
tienhigo <tienhigo@gmail.com>
2024-06-18 07:30:52 +0700

    last test today

--- frontend/src/components/todo.js
+++ frontend/src/components/todo.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function todo({}){
     return <div>
-        <h1 data-testid="todo-1">CICD</h1>
+        <h1 data-testid="todo-1">CI/CD PIPELINE</h1>
       <h1>My frontend page look like this</h1>
       </div>
 }
