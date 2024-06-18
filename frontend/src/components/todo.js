7ed2b2f05d4edb8a952b1275b5a346aa527ec767
tienhigo <tienhigo@gmail.com>
2024-06-18 11:04:58 +0700

    mess

--- frontend/src/components/todo.js
+++ frontend/src/components/todo.js
@@ -4,7 +4,8 @@ function todo({}){
     return <div>
         <h1 data-testid="todo-1">CICD</h1>
       <h1>My frontend page look like this</h1>
-      <h2>Hello world</h2>
+      <h2>Hello PTIT</h2>
+      <h2>hello world</h2>
       </div>
 }
 export default todo;
