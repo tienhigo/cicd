0d064e1b8e7f4ca166d452e4f5f0abc40744b2af
tienhigo <tienhigo@gmail.com>
2024-06-18 10:35:54 +0700

    ll

--- frontend/src/components/todo.js
+++ frontend/src/components/todo.js
@@ -2,8 +2,9 @@ import React from "react";
 
 function todo({}){
     return <div>
-        <h1 data-testid="todo-1">CI/CD PIPELINE</h1>
+        <h1 data-testid="todo-1">CI/CD PIPELINE DEMO</h1>
       <h1>My frontend page look like this</h1>
+      <h2>Hello world</h2>
       </div>
 }
 export default todo;
